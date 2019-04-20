c07af2f117694b042e94d691b48f0b4d5c8df987
Sanjar <usanjar31@gmail.com>
2019-04-20 19:34:33 +0500

    travis test

--- config/db.php
+++ config/db.php
@@ -1,21 +1,14 @@
 <?php
 
 return [
-    'class' => yii\db\Connection::class,
-    'dsn' => 'pgsql:host=127.0.0.1;port=5432;dbname=yii2_base',
-    'username' => 'sanjar',
-    'password' => 'password',
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_base',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
-    'tablePrefix' => '',
-    'schemaMap' => [
-        'pgsql' => [
-            'class' => yii\db\pgsql\Schema::class,
-            'defaultSchema' => 'public'
-        ]
-    ]
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
